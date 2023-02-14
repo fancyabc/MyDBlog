481496e1cd2f6ff2e2e3073f4d1ec8c42ffcf4d4
fanxi <2310403052@qq.com>
2023-02-14 19:44:38 +0800

    锚点

--- MyBlog/comment/views.py
+++ MyBlog/comment/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render, get_object_or_404, redirect
 from django.contrib.auth.decorators import login_required
-from django.http import HttpResponse
+from django.http import HttpResponse, JsonResponse
 from django.contrib.auth.models import User
 
 from notifications.signals import notify
@@ -37,7 +37,8 @@ def post_comment(request, article_id, parent_comment_id=None):
                         action_object=new_comment,
                     )
 
-                return HttpResponse('200 OK')
+                return JsonResponse({"code": "200 OK", "new_comment_id": new_comment.id})
+
             new_comment.save()
             if not request.user.is_superuser:
                 notify.send(
@@ -48,7 +49,9 @@ def post_comment(request, article_id, parent_comment_id=None):
                     action_object=new_comment,
                 )
 
-            return redirect(article)
+            # 添加锚点
+            redirect_url = article.get_absolute_url() + '#comment_elem_' + str(new_comment.id)
+            return redirect(redirect_url)
         else:
             return HttpResponse("表单内容有误，请重新填写。")
     # 处理错误请求
